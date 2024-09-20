3e29dce7d1bb160f926f56d5d877250812ff78a4
Katie <katherinerlim@gmail.com>
2024-09-19 21:10:25 -0400

    word pattern

---
+++ ArraysHashing/WordPattern.java
@@ -0,0 +1,23 @@
+package ArraysHashing;
+
+import java.util.HashMap;
+
+public class WordPattern {
+    public static void main(String[] args) {
+        String pattern = "abba";
+        String s = "dog cat cat dog";
+        System.out.println(wordPattern(pattern, s));
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+        String[] strs = s.split(" ");
+        if (strs.length != pattern.length()) return false;
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if (!map.containsKey(c) && !map.containsValue(strs[i])) map.put(c, strs[i]);
+            else if (!strs[i].equals(map.get(c))) return false;
+        }
+        return true;
+    }
+}
